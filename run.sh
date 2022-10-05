6b26908be35d0a4895e7dcfed310f983ebd0ecf1
yw0208 <908435304@qq.com>
2022-10-05 21:02:54 +0800

    a new training method

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH="/root/miniconda3/bin:"$PATH
-source /root/.bashrc
-source activate
-python3 train.py --regressor pymaf_net --pretrained_checkpoint logs/pymaf_res50/pymaf_res50_as_lp3_mlp256-128-64-5_Sep21-21-10-01-CWR/checkpoints/model_best.pt --misc TRAIN.BATCH_SIZE 64
